da2f3af4b244be6efacf283ea9ea213cfcb7a32e
Eugene Zbiranik <ezbiranik@gmail.com>
2020-02-08 15:28:31 +0000

    Disobey robots.txt

--- listgen/settings.py
+++ listgen/settings.py
@@ -21,7 +21,7 @@ LOG_LEVEL = 'ERROR'
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 OPR/52.0.2871.40'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
